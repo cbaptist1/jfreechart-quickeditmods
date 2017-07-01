f55d276c5f8195cefebe852c019de6db8c2b5cd6
Chris Baptist <cbaptist1@gmail.com>
2017-07-01 12:59:28 -0400

    added all

---
+++ files.bat
@@ -0,0 +1,8 @@
+copy CategoryAxis.java c:\source_code\jfreechart-1.0.19\source\org\jfree\chart\axis
+copy ChartPanel.java c:\source_code\jfreechart-1.0.19\source\org\jfree\chart
+copy ChartViewer.java c:\source_code\jfreechart-1.0.19\source\org\jfree\chart\fx
+copy DefaultDrawingSupplier.java c:\source_code\jfreechart-1.0.19\source\org\jfree\chart\plot
+copy HistogramDataset.java c:\source_code\jfreechart-1.0.19\source\org\jfree\data\statistics
+copy WaferMapDataset.java c:\source_code\jfreechart-1.0.19\source\org\jfree\data\general
+copy WaferMapPlot.java c:\source_code\jfreechart-1.0.19\source\org\jfree\chart\plot
+copy WaferMapRenderer.java c:\source_code\jfreechart-1.0.19\source\org\jfree\chart\renderer
